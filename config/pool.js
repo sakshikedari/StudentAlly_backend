9432573223e255c6cd9ca9317fd54abfd1a2fabf
sakshi kedari <139050957+sakshikedari@users.noreply.github.com>
2025-11-11 18:08:35 +0530

    change db

--- config/pool.js
+++ config/pool.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false, 
-  },
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
