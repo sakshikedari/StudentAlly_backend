934e0885565689a3b38f98628798a770c6881a96
sakshi kedari <139050957+sakshikedari@users.noreply.github.com>
2026-04-15 16:43:00 +0530

    updatded vercel

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://student-ally-frontend.vercel.app"
+  "https://studentally.sakshikedari.tech/"
 ];
 
 app.use(
